e338c67202499e12c81c26eae6841ea1e6381c4b
Taaccii <tom1991@hotmail.it>
2026-02-14 13:08:33 +0100

    feat: add logic for draw and resize the grid

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
 
 const container = document.querySelector('.grid-container');
-const button = document.querySelector('.grid-selection');
+const selectionBtn = document.querySelector('.grid-selection');
 
 let gridSize = 16;
+let isDrawing = false;
+
+
+container.addEventListener('mousedown', () => {
+  isDrawing = true;   
+});
+
+document.addEventListener('mouseup', () => {
+  isDrawing = false;
+});
+
+
+selectionBtn.addEventListener('click', () => {
+  const userInput = prompt('Insert grid size (max 100)')
+
+  if (userInput === null) {
+    return;
+  }
+
+  const newSize = +userInput;
+
+  if (newSize >= 1 && newSize <= 100) {
+    createGrid(newSize);
+  } else {
+    alert("Number not valid! Only number from 1 to 100.")
+  }
+
+});
+
+function handleMouseEnter(event) {
+  event.target.classList.add('hover-effect');
+
+  if (isDrawing) {
+    event.target.style.backgroundColor = 'hsl(270, 5%, 17%)'
+  }
+}
+
+function handleMouseLeave(event) {
+  event.target.classList.remove('hover-effect');
+}
 
 function createGrid(size) {
   while (container.firstChild) {
@@ -19,21 +59,18 @@ function createGrid(size) {
     square.style.width = squareSize + 'px';
     square.style.height = squareSize + 'px';
 
-    square.addEventListener('mouseenter', () => {
-      square.classList.add('hover-effect');
-      
-      setTimeout(() => {
-        square.classList.remove('hover-effect');
-      }, 300);
+    square.addEventListener('mouseenter', handleMouseEnter);
+    square.addEventListener('mouseleave', handleMouseLeave);
+
+    square.addEventListener('mousedown', () => {
+      square.style.backgroundColor = 'hsl(270, 5%, 17%)';
     });
-
     
-
     container.appendChild(square);
   }
-
-
-
 }
+
+
+
 
 createGrid(16);
